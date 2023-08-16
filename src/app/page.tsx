dfb4925e0c6292981d250859b037ce0ed702e748
Дмитрий Шамаль <shamaldi@pik.ru>
2023-08-16 19:39:26 +0500

    add prettier & tailwind prettier plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 const getData = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 
   const data = await response.json();
 
   return data;
-}
+};
 
 export async function generateMetadata(): Promise<Metadata> {
   const posts = await getData();
   return {
     title: posts[0].title,
-  }
+  };
 }
 
 export default async function Home() {
   const posts = await getData();
   return (
-    <div>
+    <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
       <h1>hello</h1>
       <h3>давайте посмотрим немного шрифт</h3>
       {posts.map((item: any) => (
         <p key={item.id}>{item.title}</p>
       ))}
-    </div>);
+    </div>
+  );
 }
