b15ef6162861f4b23e84404199364799305effc9
Дмитрий Шамаль <shamaldi@pik.ru>
2023-08-17 20:25:36 +0500

    add tailwind colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
 
-const inter = Roboto({   
-  weight: ['400', "500", '700'],
+const inter = Roboto({
+  weight: ['400', '500', '700'],
   style: ['normal', 'italic'],
-  subsets: ['latin']
-})
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'Главная страница!',
   description: 'А это описание для главной страницы!',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ru">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Home() {
   const posts = await getData();
   return (
-    <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+    <div>
       <h1>hello</h1>
       <h3>давайте посмотрим немного шрифт</h3>
       {posts.map((item: any) => (
